41bef4550329cf2278c608d1a2e4d9b9b171cea5
krrish1310 <kumarkrrish49935@gmail.com>
2025-05-18 09:54:43 +0530

    Update stock_prediction_app.py

--- stock_prediction_app.py
+++ stock_prediction_app.py
@@ -3,7 +3,6 @@
 # Save this code to a separate file named 'stock_prediction_app.py'
 
 import streamlit as st
-import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
